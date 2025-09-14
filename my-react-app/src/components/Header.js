af73ab82d9a4690dd33ae4fa3ae6d7235ef62494
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2025-09-14 11:32:55 +0100

    Header bar updates

--- my-react-app/src/components/Header.js
+++ my-react-app/src/components/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import headerImage from '../assets/images/Header.png'; 
 import './Header.css';
 
 function Header() {
   return (
-    <header
-      className="site-header"
-      style={{ backgroundImage: `url(${headerImage})` }}
-    >
-      <div className="header-content">
-      <h1 style={{ color: '#FFEFD5' }}>Phoenix Players Earlsdon</h1>
+    <header className="site-header">
+      <div className="header-inner">
+        <div className="header-content">
+          <h1>Phoenix Players Earlsdon</h1>
+        </div>
       </div>
     </header>
   );
