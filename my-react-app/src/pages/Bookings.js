6a565526ad2387037cb63440dc44d0298f320aff
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2025-09-20 20:26:57 +0100

    Update Bookings.js

--- my-react-app/src/pages/Bookings.js
+++ my-react-app/src/pages/Bookings.js
@@ -7,7 +7,7 @@ function Bookings() {
       <Navbar />
       <div className="page-content" style={{ padding: '1rem' }}>
         <h2>Bookings</h2>
-        <p>Booking details and forms will go here.</p>
+        <p>*Booking details and forms will go here.</p>
       </div>
     </div>
   );
