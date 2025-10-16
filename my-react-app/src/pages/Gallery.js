94f05fe9c64c8b934c7e9c8aa4b2b462b5dd7e86
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2025-10-16 20:22:22 +0100

    Image uploads

--- my-react-app/src/pages/Gallery.js
+++ my-react-app/src/pages/Gallery.js
@@ -7,7 +7,7 @@ function Gallery() {
       <Navbar />
       <div className="page-content" style={{ padding: '1rem' }}>
         <h2>Gallery</h2>
-        <p>Gallery content will go here.</p>
+        <p>Below are a few photographs from previous plays- if you visit our stage on a performance night, you will see photograph boards of all of our plays from over the years!</p>
       </div>
     </div>
   );
