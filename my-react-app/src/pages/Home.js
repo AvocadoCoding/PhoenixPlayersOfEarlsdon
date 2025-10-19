7227844ebc7073365c603cebf59f9cdeb48f64e3
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2025-10-19 13:20:07 +0100

    Update Home.js

--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div>
       <Navbar />
       <div className="home-content" style={{ padding: '1rem', textAlign: 'center' }}>
-        <h2>'Forget me Knot'- Coming 29th to 31st October</h2>
+        <h2>'Forget me Knot'- Coming January 2026!</h2>
         <p>
           Robert Zeinfeld is found wandering the streets of Leicester at 4 a.m. There's no clue how he got there, 
           just a bruised head and a suspicious policeman for company. But ... has he lost his memory; 
