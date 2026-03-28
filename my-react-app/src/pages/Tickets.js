80d7af2f19d2aa68471435c44ea2e2d8548918e1
AvocadoCoding <116954089+AvocadoCoding@users.noreply.github.com>
2026-03-28 17:07:09 +0000

    Update Tickets.js

--- my-react-app/src/pages/Tickets.js
+++ my-react-app/src/pages/Tickets.js
@@ -9,11 +9,11 @@ function Tickets() {
         <h2>Tickets</h2>
         <p>Tickets £10 on the door. Please note we are only able to accept cash at this time.</p>
 
-        <p>Play runs from Wednesday 28th January to Friday 30th January.</p>
+        <p>Play runs from Wednesday 29th April to Friday 1st May.</p>
 
         <p>Doors open at 7pm, curtain at 7.30pm.</p>
 
-        <p>To reserve tickets please call 07970484925 / 07977799128</p>
+        <p>To reserve tickets please call 07977799128 / 07549168791</p>
       </div>
     </div>
   );
